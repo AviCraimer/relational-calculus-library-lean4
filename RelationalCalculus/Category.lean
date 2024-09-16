9242ba8594c101f3e3a60aa6b4d3eb572273d230
Avi Craimer <mailavi@gmail.com>
2024-09-16 07:56:51 -0400

    I got the category type class instance defined for Rel!

--- RelationalCalculus/Category.lean
+++ RelationalCalculus/Category.lean
@@ -1,25 +1,17 @@
 import RelationalCalculus.Basic
 import RelationalCalculus.Eq
+import RelationalCalculus.Quotient
 import Mathlib.Tactic
 import Mathlib.CategoryTheory.Category.Basic
+set_option pp.proofs true
 
-
-
-open Relation
+open RelationQuotient
 open CategoryTheory
-#check Category
 
-def SetObjects : Type (u+1) := Type u
-
-
--- I need to figure out how to life the constructors of Relation into the quotient type.
-
-instance : Category.{(u+1),(u+1)} (Type u) where
-  Hom := fun α β => RelationQuotient α β
-  id := fun α => Quotient.mk idR α
-  comp := fun R S => R ▹ S
-  id_comp := by
-   simp [idR]
-
-  comp_id := sorry
-  assoc := sorry
+instance : Category (Type u) where
+  Hom  (α : Type u)(β : Type u):=  RelationQuotient α β
+  id (α : Type u) := idR α
+  comp {α β γ : Type u} (R : RelationQuotient α β) (S :RelationQuotient β γ) :=  comp R S
+  id_comp := id_comp
+  comp_id := comp_id
+  assoc := comp_assoc
--- RelationalCalculus/Quotient.lean
+++ RelationalCalculus/Quotient.lean
@@ -7,17 +7,19 @@ open Relation
 -- Defines the type of relations quotiented by evaluation equivalence.
 def RelationQuotient (α β : Type u) := Quotient (@instSetoidRelation α β)
 
-def RelationQuotient.atomic {α β : Type u} (f : Relation.Pairs α β) : RelationQuotient α β :=
+
+namespace RelationQuotient
+def atomic {α β : Type u} (f : Relation.Pairs α β) : RelationQuotient α β :=
   Quotient.mk _ (Relation.atomic f)
 
-def RelationQuotient.idR (α  : Type u) : RelationQuotient α α  :=
+def idR (α  : Type u) : RelationQuotient α α  :=
   Quotient.mk _ (Relation.idR α)
-
-def RelationQuotient.pair {α β : Type u} (a : α) (b : β) : RelationQuotient α β :=
+#check idR
+def pair {α β : Type u} (a : α) (b : β) : RelationQuotient α β :=
   Quotient.mk _ (Relation.pair a b)
 
 
-def RelationQuotient.comp {α β γ : Type u} (R : RelationQuotient α β) (S : RelationQuotient β γ) : RelationQuotient α γ :=
+def comp {α β γ : Type u} (R : RelationQuotient α β) (S : RelationQuotient β γ) : RelationQuotient α γ :=
   Quotient.lift₂ (fun R' S' => Quotient.mk _ (Relation.comp R' S')) (fun R S R' S' h1 h2 => Quotient.sound (by
     have RR'eval := eq_to_eval h1
     have SS'eval := eq_to_eval h2
@@ -32,3 +34,44 @@ def RelationQuotient.comp {α β γ : Type u} (R : RelationQuotient α β) (S : 
       rw [RR'eval, SS'eval]
       exact ⟨b, ⟨R'eval, S'eval ⟩⟩
   )) R S
+
+theorem comp_id {X Y: Type u} : ∀ (Rq  : RelationQuotient X Y), comp Rq (idR Y) = Rq := by
+apply Quotient.ind
+intro R
+apply Quotient.sound
+simp [(·≈·), AntisymmRel, LE.le,Relation.eval]
+
+theorem id_comp {X Y: Type u} : ∀ (Rq  : RelationQuotient X Y), comp (idR X) Rq = Rq := by
+apply Quotient.ind
+intro R
+apply Quotient.sound
+simp [(·≈·), AntisymmRel, LE.le,Relation.eval]
+
+theorem comp_assoc {W X Y Z : Type u} :
+∀
+  (R : RelationQuotient W X)
+  (S : RelationQuotient X Y)
+  (T : RelationQuotient Y Z),
+    (comp (comp R S) T) = (comp R (comp S T)) := by
+  apply Quotient.ind
+  intro R
+  apply Quotient.ind
+  intro S
+  apply Quotient.ind
+  intro T
+  apply Quotient.sound
+  simp [(·≈·), AntisymmRel, LE.le,Relation.eval, Relation.domain]
+  constructor <;> intro a b
+  · intro y x Re Se Te
+    use x
+    constructor
+    · exact Re
+    · use y
+  · intro x Re y Se Te
+    use y
+    constructor
+    · use x
+    · exact Te
+
+
+end RelationQuotient
